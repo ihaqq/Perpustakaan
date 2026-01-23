3e3dec9d09ef8ec31ede82367059687583acd7d8
BonSXCode <cmdemode@gmail.com>
2026-01-23 08:02:19 +0700

    perbaikan web.php

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,24 @@
 use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('sampah/welcome');
-})      -> name('/');
+// Route::get('/', function () {
+//     return view('sampah/welcome');
+// })      -> name('/');
 
-Route::get('/halaman-admin', function () {
-    return view('Admin/dashboard/dashboard_improved');
-})      -> name('dashboard-admin');
+// Route::get('/halaman-admin', function () {
+//     return view('Admin/dashboard/dashboard_improved');
+// })      -> name('dashboard-admin');
 
-Route::get('/halaman-daftar-anggota', function () {
-    return view('Admin/anggota/halaman_anggota');
-});
+// Route::get('/halaman-daftar-anggota', function () {
+//     return view('Admin/anggota/halaman_anggota');
+// });
 
-Route::get('/halaman-admin-inspirasi', function () {
-    return view('Admin/template/gemini/dashboard');
-});
-Route::get('/buku', function () {
-    return view('Admin/buku/halaman_buku');
-});
+// Route::get('/halaman-admin-inspirasi', function () {
+//     return view('Admin/template/gemini/dashboard');
+// });
+// Route::get('/buku', function () {
+//     return view('Admin/buku/halaman_buku');
+// });
 
 
 // testing
